75dee34b6da21096389c45dcb2401372dbb9d34f
ElliotThiebaut <hello@elliotthiebaut.com>
2024-10-11 22:14:26 -0400

    Pined TS to 5.5.4 to stay in eslint supported range

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -17,9 +17,7 @@ export default interface Person extends WritableDbItem {
 export enum UserRoles {
   CLIENT = 1 << 0,
   STAFF = 1 << 1,
-  // @ts-expect-error Valid range
   DEVELOPER = 1 << 51,
-  // @ts-expect-error Valid range
   BOT = 1 << 52,
 }
 
