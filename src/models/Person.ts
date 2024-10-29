ffdc3101944fe18ff5a437e6ff646ffbd99e4316
ElliotThiebaut <hello@elliotthiebaut.com>
2024-10-28 23:54:30 -0400

    Updated dependencies

--- src/models/Person.ts
+++ src/models/Person.ts
@@ -17,7 +17,9 @@ export default interface Person extends WritableDbItem {
 export enum UserRoles {
   CLIENT = 1 << 0,
   STAFF = 1 << 1,
+  //@ts-expect-error - valid range
   DEVELOPER = 1 << 51,
+  //@ts-expect-error - valid range
   BOT = 1 << 52,
 }
 
