041c9a6735c7b15d4faa388b1fc23832a3fa7630
Elliot Thiebaut <hello@elliotthiebaut.com>
2024-10-29 11:03:39 -0400

    Detailed auth and tokens logs (#81)
    * Updated dependencies
    
    * Added detailed logs to tokens fetching

--- src/api/endpoints/auth/TokenManager.ts
+++ src/api/endpoints/auth/TokenManager.ts
@@ -28,42 +28,59 @@ export default class TokenManager extends ApiEndpoint {
     return "auth";
   }
 
-  public async getAccessToken(): Promise<string | undefined> {
+  public async getAccessToken(source: string = "unknown"): Promise<string | undefined> {
+    this.log?.debug(`getAccessToken: New token requested from ${source}`);
     if (this.accessToken !== undefined) {
       const exp = TokenManager.getJwtFromAccessToken(this.accessToken).exp;
 
       // check expiration minus 5 minutes to guard against race condition or timing issues creating unnecessary 403s
-      if (exp > Date.now() / 1000 - 60 * 5) return this.accessToken;
+      if (exp > Date.now() / 1000 - 60 * 5) {
+        this.log?.debug(`getAccessToken: ${source} => Token valid and simply returned`);
+        return this.accessToken;
+      }
     }
 
     // if the refresh token isn't present, nothing we can do
-    if (this.refreshToken === undefined) return this.accessToken;
+    if (this.refreshToken === undefined) {
+      this.log?.debug(`getAccessToken: ${source} => Missing refresh token, returning stored access token`);
+      return this.accessToken;
+    }
 
     // token is expired (or will expire soon) and there is not already one fetching, so fetch a new one
-    if (!this.tokenFetchPromise) this.tokenFetchPromise = this.fetchToken(this.refreshToken);
+    if (!this.tokenFetchPromise) {
+      this.log?.debug(`getAccessToken: ${source} => No current token fetch, starting new fetch`);
+      this.tokenFetchPromise = this.fetchToken(this.refreshToken, source);
+    }
 
     try {
+      this.log?.debug(`getAccessToken: ${source} => Waiting for token fetch to complete`);
       const tokens = await this.tokenFetchPromise;
+
+      this.log?.debug(`getAccessToken: ${source} => Setting new tokens in memory`);
       this.accessToken = tokens.accessToken;
       this.refreshToken = tokens.refreshToken;
 
       if (this.refreshCallback !== undefined) {
+        this.log?.debug(`getAccessToken: ${source} => Calling refresh callback with new tokens`);
         await this.refreshCallback(tokens);
       }
     } finally {
       this.tokenFetchPromise = undefined;
     }
 
+    this.log?.debug(`getAccessToken: ${source} => Returning new access token`);
     return this.accessToken;
   }
 
-  private async fetchToken(refreshToken: string): Promise<TokenGrant> {
+  private async fetchToken(refreshToken: string, source: string = "unknown"): Promise<TokenGrant> {
+    this.log?.debug(`getAccessToken: ${source} => Fetching new tokens`);
     const result = await this.postRequest<TokenGrant>("/exchange", { refreshToken: refreshToken }, true);
 
     if (!result.success || result.data === undefined) {
       throw Error(result.statusCode?.toString());
     }
 
+    this.log?.debug(`getAccessToken: ${source} => Successfully fetched new tokens`);
     return result.data;
   }
 
