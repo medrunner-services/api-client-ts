a421acee9aa004dc4c5ae36aa26aff23756f83b4
ElliotThiebaut <hello@elliotthiebaut.com>
2025-03-21 22:19:40 -0400

    Removed unused type

--- src/api/endpoints/emergency/response/ClientDetailsResponse.ts
+++
@@ -1,9 +0,0 @@
-import { PersonType } from "../../../../models/Person";
-
-export default interface ClientDetailsResponse {
-  totalEmergencies: number;
-  emergencySuccessRate: number;
-  joinDate: string;
-  personType: PersonType;
-  hasCitizenCon2954Promo: boolean;
-}
