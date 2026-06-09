35d381413e9ef7db1c4cf4839fd9170acaf1d9ae
ElliotThiebaut <hello@elliotthiebaut.com>
2026-06-09 10:38:59 +0200

    chore: update dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -28,10 +28,11 @@ module.exports = {
       (See: https://git.coolaj86.com/coolaj86/semver-utils.js#semverutils-parse-semverstring)
      @returns - One of the valid target values (specified in the table above).
      */
+  // We return greatest and not latest to make cooldown work with the latest version that is old enough
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/explicit-function-return-type
   target: (name, semver) => {
     if (name.startsWith("@medrunner")) return "@beta";
     if (name === "@types/node") return "minor";
-    return "latest";
+    return "greatest";
   },
 };
